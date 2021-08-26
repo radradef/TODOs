83571a0f329f416f684275b1bdd8003cd992c1a2
rradev <radoslavr@securegroup.com>
2021-08-26 21:55:20 +0300

    init

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Header />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
+
+export default App;
