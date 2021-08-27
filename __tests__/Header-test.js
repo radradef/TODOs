694b0dcaeb5f8b4126cd9a59fee486e33ee5f584
rradev <radoslavr@securegroup.com>
2021-08-27 22:31:21 +0300

    Header test

---
+++ __tests__/Header-test.js
@@ -0,0 +1,15 @@
+/**
+ * @format
+ */
+
+ import 'react-native';
+ import React from 'react';
+ import Header from '../components/Header';
+ import { expect } from '@jest/globals';
+ import renderer from 'react-test-renderer';
+ 
+ it('matches snapshot', () => {
+   const tree = renderer.create(<Header />).toJSON();
+   expect(tree).toMatchSnapshot();
+ });
+ 
